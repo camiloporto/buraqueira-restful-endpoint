c54fd90ca5b0acfafe506fbeb1e946cb7c90de93
Camilo Porto <camiloporto@gmail.com>
2016-04-10 16:13:53 -0300

    started project with spring boot initializer

---
+++ src/main/java/br/com/camiloporto/buraqueira/BuraqueiraRestfulEndpointApplication.java
@@ -0,0 +1,12 @@
+package br.com.camiloporto.buraqueira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuraqueiraRestfulEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuraqueiraRestfulEndpointApplication.class, args);
+	}
+}
